b6e0d5effc36b4be5c94eced006bbb4aa7e19f16
ggornes <gerardogornes@gmail.com>
2020-06-02 10:32:02 +0800

    Generated rate seeds using Rates Factory

--- database/factories/RateFactory.php
+++ database/factories/RateFactory.php
@@ -8,5 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Rate::class, function (Faker $faker) {
     return [
         //
+        'rate' => $faker->numberBetween(100,5000),
+        'description' => $faker->sentence(3)
     ];
 });
